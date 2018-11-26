fd114b2704bd49b5c05c95a55fc030d3123a5c68
tmosest <tmosest@gmail.com>
2018-11-25 19:29:31 -0500

    example of an async task

--- app/src/main/java/com/tmosest/top10downloads/MainActivity.kt
+++ app/src/main/java/com/tmosest/top10downloads/MainActivity.kt
@@ -1,12 +1,35 @@
 package com.tmosest.top10downloads
 
+import android.os.AsyncTask
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 
 class MainActivity : AppCompatActivity() {
+    private val TAG = "MainActivity"
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        Log.d(TAG, "onCreate: called")
+        val downloadData = DownloadData()
+        downloadData.execute("URL goes here")
+        Log.d(TAG, "onCreate: done")
+    }
+
+    companion object {
+        private class DownloadData : AsyncTask<String, Void, String>() {
+            private val TAG = "DownloadData"
+            override fun doInBackground(vararg p0: String?): String {
+                Log.d(TAG, "onPostExecute parameter is ${p0[0]}")
+                return "doInBackgroundComplete"
+            }
+
+            override fun onPostExecute(result: String?) {
+                super.onPostExecute(result)
+                Log.d(TAG, "onPostExecute parameter is $result")
+            }
+        }
     }
 }
