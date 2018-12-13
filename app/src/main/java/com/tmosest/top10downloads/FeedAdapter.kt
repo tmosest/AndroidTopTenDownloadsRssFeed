944f5a97da3d669dd91278eae2e9c775925b1492
tmosest <tmosest@gmail.com>
2018-12-13 00:19:11 -0500

    list view holder example

--- app/src/main/java/com/tmosest/top10downloads/FeedAdapter.kt
+++ app/src/main/java/com/tmosest/top10downloads/FeedAdapter.kt
@@ -8,23 +8,41 @@ import android.view.ViewGroup
 import android.widget.ArrayAdapter
 import android.widget.TextView
 
+/**
+ * Example of a view holder pattern
+ */
+class ViewHolder(v: View) {
+    val tvName: TextView = v.findViewById(R.id.tvName)
+    val tvArtist: TextView = v.findViewById(R.id.tvArtist)
+    val tvSummary: TextView = v.findViewById(R.id.tvSummary)
+}
+
+/**
+ * Example of a custom adapter
+ */
 class FeedAdapter(context: Context, val resource: Int, val entries: List<FeedEntry>) : ArrayAdapter<FeedEntry>(context, resource, entries) {
     private val tag = "FeedAdapter"
     private val inflater = LayoutInflater.from(context)
 
     override fun getView(position: Int, convertView: View?, parent: ViewGroup): View {
         Log.d(tag, "reused a view ${convertView != null}")
-        val view: View = convertView ?: inflater.inflate(resource, parent, false)
+        val view: View
+        val viewHolder: ViewHolder
 
-        val tvName = view.findViewById<TextView>(R.id.tvName)
-        val tvArtist = view.findViewById<TextView>(R.id.tvArtist)
-        val tvSummary = view.findViewById<TextView>(R.id.tvSummary)
+        if (convertView == null) {
+            view = inflater.inflate(resource, parent, false)
+            viewHolder = ViewHolder(view)
+            view.tag = viewHolder
+        } else {
+            view = convertView
+            viewHolder = view.tag as ViewHolder
+        }
 
         val currentEntry = entries[position]
 
-        tvName.text = currentEntry.name
-        tvArtist.text = currentEntry.artist
-        tvSummary.text = currentEntry.summary
+        viewHolder.tvName.text = currentEntry.name
+        viewHolder.tvArtist.text = currentEntry.artist
+        viewHolder.tvSummary.text = currentEntry.summary
 
         return view
     }
