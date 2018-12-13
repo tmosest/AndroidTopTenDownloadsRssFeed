35277ebcb95ae2770812ee6064e843bc31f34311
tmosest <tmosest@gmail.com>
2018-12-12 21:57:37 -0500

    show top 25 downloads as a list

--- app/src/main/java/com/tmosest/top10downloads/DownloadData.kt
+++ app/src/main/java/com/tmosest/top10downloads/DownloadData.kt
@@ -1,11 +1,24 @@
 package com.tmosest.top10downloads
 
+import android.content.Context
 import android.os.AsyncTask
 import android.util.Log
+import android.widget.ArrayAdapter
+import android.widget.ListView
 import java.net.URL
+import kotlin.properties.Delegates
 
-class DownloadData : AsyncTask<String, Void, String>() {
+class DownloadData(context: Context, listView: ListView) : AsyncTask<String, Void, String>() {
     private val tag = "DownloadData"
+
+    private var context: Context by Delegates.notNull()
+    private var listView: ListView by Delegates.notNull()
+
+    init {
+        this.context = context
+        this.listView = listView
+    }
+
     override fun doInBackground(vararg urls: String?): String {
         Log.d(tag, "doInBackground parameter is ${urls[0]}")
         val rssFeed = downloadXML(urls[0])
@@ -20,6 +33,9 @@ class DownloadData : AsyncTask<String, Void, String>() {
         // Log.d(tag, "onPostExecute parameter is $result")
         val parseApplication = ParseApplication()
         parseApplication.parse(result)
+
+        val arrayAdapter = ArrayAdapter<FeedEntry>(context, R.layout.list_item, parseApplication.applications)
+        listView.adapter = arrayAdapter
     }
 
     private fun downloadXML(urlPath: String?): String {
--- app/src/main/java/com/tmosest/top10downloads/MainActivity.kt
+++ app/src/main/java/com/tmosest/top10downloads/MainActivity.kt
@@ -3,16 +3,16 @@ package com.tmosest.top10downloads
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
     private val tag = "MainActivity"
-    private val appleTop10Downloads = "http://ax.itunes.apple.com/WebObjects/MZStoreServices.woa/ws/RSS/topfreeapplications/limit=10/xml"
+    private val appleTop10Downloads = "http://ax.itunes.apple.com/WebObjects/MZStoreServices.woa/ws/RSS/topfreeapplications/limit=25/xml"
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-
         Log.d(tag, "onCreate: called")
-        val downloadData = DownloadData()
+        val downloadData = DownloadData(this, lvTopDownloads)
         downloadData.execute(appleTop10Downloads)
         Log.d(tag, "onCreate: done")
     }
