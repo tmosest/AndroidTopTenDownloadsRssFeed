9b1dd78ba2559d53142821da39a135d0dd7b4d44
tmosest <tmosest@gmail.com>
2018-12-13 00:45:59 -0500

    menu can now change feeds

--- app/src/main/java/com/tmosest/top10downloads/MainActivity.kt
+++ app/src/main/java/com/tmosest/top10downloads/MainActivity.kt
@@ -3,23 +3,46 @@ package com.tmosest.top10downloads
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
+import android.view.Menu
+import android.view.MenuItem
 import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
     private val tag = "MainActivity"
-    private val appleTop10Downloads = "http://ax.itunes.apple.com/WebObjects/MZStoreServices.woa/ws/RSS/topfreeapplications/limit=25/xml"
-    private val downloadData: DownloadData by lazy { DownloadData(this, lvTopDownloads) }
+    private var downloadData: DownloadData? = null
+
+    private fun downLoadAppleInformation(feedOption: String = "topfreeapplications") {
+        val url = "http://ax.itunes.apple.com/WebObjects/MZStoreServices.woa/ws/RSS/$feedOption/limit=25/xml"
+        downloadData = DownloadData(this, lvTopDownloads)
+        downloadData?.execute(url)
+    }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
         Log.d(tag, "onCreate: called")
-        downloadData.execute(appleTop10Downloads)
+        downLoadAppleInformation()
         Log.d(tag, "onCreate: done")
     }
 
     override fun onDestroy() {
         super.onDestroy()
-        downloadData.cancel(true)
+        downloadData?.cancel(true)
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        menuInflater.inflate(R.menu.feeds_menu, menu)
+        return true
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        val feedOption = when (item.itemId) {
+            R.id.mnuFree    -> "topfreeapplications"
+            R.id.mnuPaid    -> "toppaidapplications"
+            R.id.mnuSongs   -> "topsongs"
+            else -> return super.onOptionsItemSelected(item)
+        }
+        downLoadAppleInformation(feedOption)
+        return true
     }
 }
